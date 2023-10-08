3b3c25a0ec384432f72e0f4416164515d2b41369
Zero <bouchghlahmed@gmail.com>
2023-10-08 18:57:34 +0100

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './CSS/index.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Kasa">
       <App />
     </BrowserRouter>
   </React.StrictMode>
